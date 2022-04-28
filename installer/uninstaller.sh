7c985c8f7bd5a4e5a596f56e8db7a37631f9cbee
Nannan <nanna7077@gmail.com>
2022-04-28 11:10:18 +0530

    Change: Window dimensions

--- installer/uninstaller.sh
+++ installer/uninstaller.sh
@@ -2,7 +2,6 @@
 
 echo "Uninstalling Aoede"
 echo ""
-pip uninstall -y -r /opt/aoede/requirements.txt
 rm -rf /opt/aoede
 rm /usr/share/applications/aoede.desktop
 echo ""
--- src/define.py
+++ src/define.py
@@ -1,7 +1,7 @@
 import os
 
-WINDOW_WIDTH=410
-WINDOW_HEIGHT=700
+WINDOW_WIDTH=350
+WINDOW_HEIGHT=595
 GENIUS_API_TOKEN="_Pe-HxIj6CNGImdRIcenX_4D51FvShKKQlQDP52NSMfcsOKaDd3r1CguBtbuT4ht"
 if os.name=="posix":
     DB_PATH=os.path.expanduser('~/.config/aoede.db')
